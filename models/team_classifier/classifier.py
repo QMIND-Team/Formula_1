cf78023722d570ee2d5edc39b899ccf3eaffd2e4
eyuyitung <eyuyitung@gmail.com>
2020-02-09 23:42:59 -0500

    updated generateHistogram to write histogram data to CSVs as eventual input to classifier model.

--- models/team_classifier/classifier.py
+++ models/team_classifier/classifier.py
@@ -1,31 +1,33 @@
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
 import cv2
 import os
 
-def generate1DimHistogram (imgs):
+# GLOBALS
+output_folder_path = ".." + os.path.sep + ".." + os.path.sep + "data" + os.path.sep + "team_on_screen" + os.path.sep
+
+# generates histograms for images in ndarray form.
+# data written out to csv, 256 x 3.
+# 256 col corresponding to the pixel count in each of the
+# 256 pixel intensity bins as a percentage of the total pixel count
+# in that channel.
+# 3 row for the corresponding values in each of the red green and blue
+# components.
+# input should be cropped images, without background to minimize noise.
+def generateHistogram(imgs):
     for img in imgs:
+        hists = []
         chans = cv2.split(img[0])
         colors = ("b", "g", "r")
-
-        plt.figure()
-        plt.title(img[1])
-        plt.xlabel("bins")
-        plt.ylabel("# of pixels")
-        features = []
-
-        # loop through image channels
         for(chan, color) in zip(chans, colors):
-            # create histogram for current channel and concat resulting hist
-            # for each channel
-            hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
-            features.extend(hist)
+            hist = cv2.calcHist([chan], [0], None, [256], [0, 256]).flatten()
+            hist = hist / chan.size
+            hists.append(hist)
+        hist_arr = np.array(hists)
+        np.savetxt(output_folder_path + img[1].split(".jpg")[0] + ".csv", hist_arr, delimiter=',')
 
-            # plot the histogram
-            plt.plot(hist, color=color)
-            plt.xlim([0,256])
-        plt.show()
-
+# read in images into (ndarray form, file name) tuple
 def load_images(folder):
     images = []
     for filename in os.listdir(folder):
@@ -34,9 +36,11 @@ def load_images(folder):
             images.append((img, filename))
     return images
 
+
 def main():
     imgs = load_images("test_imgs")
-    generate1DimHistogram(imgs)
+    generateHistogram(imgs)
+
 
 if __name__ == "__main__":
     main()
